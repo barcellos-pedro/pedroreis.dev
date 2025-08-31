37440aad605fb3411bfc74129f65d0a2fd6fe7b0
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-08-30 21:31:07 -0300

    updaet k6 load testing script

--- load-test.js
+++ load-test.js
@@ -2,29 +2,19 @@
 // https://grafana.com/docs/k6/latest/
 
 import http from "k6/http"
-import { sleep } from "k6"
+import { sleep, check } from "k6"
 
 export const options = {
   gracefulRampDown: "5s",
   stages: [
-    { duration: "30s", target: 20 },
-    { duration: "1m30s", target: 10 },
-    { duration: "20s", target: 0 },
-  ],
-  summaryTrendStats: [
-    "avg",
-    "min",
-    "med",
-    "max",
-    "p(90)",
-    "p(95)",
-    "p(99.99)",
-    "count",
+    { duration: "10s", target: 5 },
+    { duration: "1m15s", target: 10 },
+    { duration: "30s", target: 30 },
   ],
 }
 
 export default function () {
-  http.get("https://pedroreis.dev")
+  const res = http.get("https://pedroreis.dev")
   check(res, { "status was 200": (r) => r.status == 200 })
   sleep(1)
 }
