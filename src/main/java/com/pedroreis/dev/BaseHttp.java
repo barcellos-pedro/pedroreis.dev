9451cc6fdb7a09264d043d4cd0cfd87ac0854692
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-09-03 17:59:53 -0300

    Create base class for RepoController

---
+++ src/main/java/com/pedroreis/dev/BaseHttp.java
@@ -0,0 +1,45 @@
+package com.pedroreis.dev;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class BaseHttp {
+    protected HttpClient getHttpClient() {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofMillis(500))
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+    }
+
+    protected HttpRequest getRequest(String url) throws URISyntaxException {
+        return HttpRequest.newBuilder()
+                .uri(getUri(url))
+                .GET()
+                .build();
+    }
+
+    protected URI getUri(String url) throws URISyntaxException {
+        return new URI(url);
+    }
+
+    protected List<Repo> parse(HttpResponse<String> response) throws JsonProcessingException, JsonMappingException {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .setPropertyNamingStrategy(SNAKE_CASE)
+                .readValue(response.body(), new TypeReference<List<Repo>>() {
+                });
+    }
+}
--- src/main/java/com/pedroreis/dev/RepoController.java
+++ src/main/java/com/pedroreis/dev/RepoController.java
@@ -1,14 +1,10 @@
 package com.pedroreis.dev;
 
+import static java.net.http.HttpResponse.BodyHandlers.ofString;
+
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,50 +12,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 @Controller
-public class RepoController {
-    private static final Logger log = LoggerFactory.getLogger(RepoController.class);
+public class RepoController extends BaseHttp {
+        private static final Logger log = LoggerFactory.getLogger(RepoController.class);
+        public static final String URL = "https://api.github.com/users/barcellos-pedro/repos?sort=created&per_page=100&page=1";
 
-    private final HttpClient http;
+        @GetMapping("/projects")
+        String index(Model model) throws IOException, InterruptedException, URISyntaxException {
+                var request = getRequest(URL);
+                var http = getHttpClient();
+                
+                var response = http.send(request, ofString());
+                log.info("[Repos:Status Code] {}", response.statusCode());
 
-    public RepoController() {
-        http = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofMillis(500))
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .build();
-    }
+                List<Repo> repos = parse(response);
+                model.addAttribute("repos", repos);
 
-    @GetMapping("/projects")
-    String index(Model model) throws IOException, InterruptedException, URISyntaxException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://api.github.com/users/barcellos-pedro/repos?sort=created&per_page=100&page=1"))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = http.send(request, HttpResponse.BodyHandlers.ofString());
-
-        String body = response.body();
-
-        log.info("Repos Response Status Code: {}", response.statusCode(), body);
-
-        List<Repo> projects = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
-                .readValue(response.body(), new TypeReference<List<Repo>>() {
-                });
-
-        int quantity = projects.size();
-        log.info("Projects {}", quantity);
-
-        model.mergeAttributes(Map.of(
-                "projects", projects,
-                "quantity", quantity));
-
-        return "projects";
-    }
+                return "projects";
+        }
 }
