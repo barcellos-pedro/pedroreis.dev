9a217f8a07f927106901e4022a0cb20e85a2eb8e
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-09-05 19:06:59 -0300

    format document and optimize imports

--- src/main/java/com/pedroreis/dev/model/Repo.java
+++ src/main/java/com/pedroreis/dev/model/Repo.java
@@ -1,33 +1,41 @@
 package com.pedroreis.dev.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.Instant;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.pedroreis.dev.utils.DateUtils;
 
-/// Repository
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+
+/// Repository model
+/// Used to map the JSON response from GitHub API
+/// and also to map the database result set
+///
+/// Fields:
 /// - name      = {repo_name}
 /// - fullName  = {user/repo_name}
-/// - htmlUrl   = repository github page
+/// - htmlUrl   = repository GitHub page
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Repo(
-                String name,
-                String htmlUrl,
-                String description,
-                Instant createdAt,
-                List<String> topics) {
+        String name,
+        String htmlUrl,
+        String description,
+        Instant createdAt,
+        List<String> topics) {
 
-        public static Repo of(ResultSet resultSet) throws SQLException {
-                var createdAt = DateUtils.getDate(resultSet.getString("createdAt"));
-                var topics = List.of(resultSet.getString("topic_list").split(", "));
+    public static Repo of(ResultSet resultSet) throws SQLException {
+        var createdAt = DateUtils.getDate(resultSet.getString("createdAt"));
+        var topicList = resultSet.getString("topic_list");
+        List<String> topics = topicList.isBlank() ?
+                Collections.emptyList() :
+                List.of(topicList.split(", "));
 
-                return new Repo(resultSet.getString("repo_name"),
-                                resultSet.getString("htmlUrl"),
-                                resultSet.getString("description"),
-                                createdAt,
-                                topics);
-        }
+        return new Repo(resultSet.getString("repo_name"),
+                resultSet.getString("htmlUrl"),
+                resultSet.getString("description"),
+                createdAt,
+                topics);
+    }
 }
