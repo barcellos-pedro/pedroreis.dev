20eb90215a3421b493f6bd1b34226be09b2745b8
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-09-05 09:51:39 -0300

    Get Repos from Database instead of API request

--- src/main/java/com/pedroreis/dev/controller/repos/RepoController.java
+++ src/main/java/com/pedroreis/dev/controller/repos/RepoController.java
@@ -1,13 +1,12 @@
 package com.pedroreis.dev.controller.repos;
 
-import static java.net.http.HttpResponse.BodyHandlers.ofString;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,20 +15,23 @@ import com.pedroreis.dev.model.Repo;
 
 @Controller
 public class RepoController extends BaseHttp {
-        private static final Logger log = LoggerFactory.getLogger(RepoController.class);
-        public static final String URL = "https://api.github.com/users/barcellos-pedro/repos?sort=created&per_page=100&page=1";
+        private static final Logger LOG = LoggerFactory.getLogger(RepoController.class);
+        private final JdbcTemplate jdbcTemplate;
+
+        public RepoController(JdbcTemplate jdbcTemplate) {
+                this.jdbcTemplate = jdbcTemplate;
+        }
 
         @GetMapping("/projects")
         String index(Model model) throws IOException, InterruptedException, URISyntaxException {
-                var request = getRequest(URL);
-                var http = getHttpClient();
-                
-                var response = http.send(request, ofString());
-                log.info("[Repos:Status Code] {}", response.statusCode());
-
-                List<Repo> repos = parse(response);
+                List<Repo> repos = getRepos();
+                LOG.info("[DB:Repos]: {}", repos.size());
                 model.addAttribute("repos", repos);
-
                 return "repos/index";
+        }
+
+        private List<Repo> getRepos() {
+                return jdbcTemplate.query("SELECT * FROM repos",
+                                (resultSet, rowNum) -> Repo.of(resultSet));
         }
 }
