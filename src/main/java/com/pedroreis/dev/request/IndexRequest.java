6d42cd34a656bc8dedff682610d60a8c57fa6a3d
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-09-05 19:06:43 -0300

    optimize imports

--- src/main/java/com/pedroreis/dev/request/IndexRequest.java
+++ src/main/java/com/pedroreis/dev/request/IndexRequest.java
@@ -1,11 +1,10 @@
 package com.pedroreis.dev.request;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 public record IndexRequest(String userAgent, String path, String ipAddress) {
-    
+
     public static IndexRequest of(HttpServletRequest request) {
         String userAgent = request.getHeader(HttpHeaders.USER_AGENT);
         String path = request.getRequestURI();
