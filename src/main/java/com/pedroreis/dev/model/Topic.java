fdb5dbca88199827a77c07d86406f438e5fa28b0
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-09-24 17:42:18 -0300

    add overloaded method to create Topic

--- src/main/java/com/pedroreis/dev/model/Topic.java
+++ src/main/java/com/pedroreis/dev/model/Topic.java
@@ -1,15 +1,21 @@
 package com.pedroreis.dev.model;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Topic extends ActiveRecord {
+
     public Topic() {
     }
 
-    public static void create(Repo repo) {
-        var id = Repo.getId(repo);
-        var topics = String.join(", ", repo.topics());
-        jdbcTemplate.update("INSERT INTO topics (topic_list, repo_id) VALUES (?, ?)", topics, id);
+    public static void create(String topics, String repoId) {
+        jdbcTemplate.update("INSERT INTO topics (topic_list, repo_id) VALUES (?, ?)", topics, repoId);
     }
-}
+
+    public static void create(List<String> topics, String repoId) {
+        String topicList = String.join(", ", topics);
+        create(topicList, repoId);
+    }
+    }
