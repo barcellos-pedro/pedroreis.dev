8fc4500e0092a281abbabf42ca18aaadaecf2f8b
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-09-24 20:07:26 -0300

    add @Profile("dev") to CommandLineRunner bean

--- src/main/java/com/pedroreis/dev/config/DatabaseConfig.java
+++ src/main/java/com/pedroreis/dev/config/DatabaseConfig.java
@@ -2,7 +2,6 @@ package com.pedroreis.dev.config;
 
 import com.pedroreis.dev.GithubClient;
 import com.pedroreis.dev.model.Repo;
-import jakarta.annotation.PostConstruct;
 
 import java.util.List;
 
@@ -11,24 +10,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DatabaseConfig {
     public static final Logger LOG = LoggerFactory.getLogger(DatabaseConfig.class);
     private final GithubClient githubClient;
-
+    
     public DatabaseConfig(GithubClient githubClient) {
         this.githubClient = githubClient;
     }
-
-    @PostConstruct
-    public void onInit() {
-        LOG.info("[Config:DB] Updating Tables...");
-    }
-
+    
     @Bean
+    @Profile("dev")
     CommandLineRunner seedTables() {
         return args -> {
+            LOG.info("[Config:DB] Updating Tables...");
             List<Repo> repos = githubClient.fetchRepos();
             LOG.info("Total Repos: {}", repos.size());
             repos.forEach(Repo::save);            
