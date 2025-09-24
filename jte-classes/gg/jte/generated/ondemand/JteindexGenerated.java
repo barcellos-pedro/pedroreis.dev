a18e5f49955b23be7e6a9b2b909ca602d52331c7
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2025-09-24 17:39:09 -0300

    jte genereated classes

--- jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -1,15 +1,15 @@
 package gg.jte.generated.ondemand;
-import com.pedroreis.dev.Link;
+import com.pedroreis.dev.model.Link;
 import java.util.List;
 @SuppressWarnings("unchecked")
-public final class JtehomeGenerated {
-	public static final String JTE_NAME = "home.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,2,2,2,4,4,6,6,55,55,55,55,56,56,56,56,56,56,56,56,56,56,56,56,56,56,56,56,56,57,57,57,59,59,59,61,61,64,64,64,66,66,66,2,2,2,2};
+public final class JteindexGenerated {
+	public static final String JTE_NAME = "index.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,1,3,3,3,3,5,5,7,7,19,19,19,19,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,21,21,21,23,23,23,25,25,28,28,28,29,29,29,3,3,3,3};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, List<Link> links) {
 		jteOutput.writeContent("\n");
 		gg.jte.generated.ondemand.JtelayoutGenerated.render(jteOutput, jteHtmlInterceptor, "Home", new gg.jte.html.HtmlContent() {
 			public void writeTo(gg.jte.html.HtmlTemplateOutput jteOutput) {
-				jteOutput.writeContent("\n    <style>\n        .bio {\n            display: flex;\n            flex-direction: column;\n            align-items: center;\n            justify-content:center;\n            height: 100vh;\n            \n            img {\n                width:112px;\n                height:112px;\n                border-radius: 50%;\n            }\n            \n            h3 {\n                margin-top: 0px;\n                text-align: center;\n            \n                p {\n                    margin-bottom: 0.5em;\n                    font-weight: bold;\n                    margin-top: 2em;\n                }\n                p:nth-of-type(2) {\n                    margin-top: 0;\n                    font-weight: 300;\n                }\n            }\n\n            .links {\n                display:flex;\n                gap:3em;\n                align-items:center;\n            }\n        }\n    </style>\n\n    <div class=\"bio\">\n        <img src=\"https://avatars.githubusercontent.com/u/33139500?v=4\" alt=\"Pedro's profile image\">\n\n        <h3>\n            <p>Pedro Barcellos dos Reis</p>\n            <p>Software Engineer</p>\n        </h3>\n\n        <a href=\"/projects\">Check out my Portfolio Overview &rarr;</a>\n\n        <section class=\"links\">\n            ");
+				jteOutput.writeContent("\n    <div class=\"bio\">\n        <img src=\"https://avatars.githubusercontent.com/u/33139500?v=4\" alt=\"Pedro's profile image\">\n\n        <h3>\n            <p>Pedro Barcellos dos Reis</p>\n            <p>Software Engineer</p>\n        </h3>\n\n        <a href=\"/projects\">Check out my Portfolio Overview &rarr;</a>\n\n        <section class=\"links\">\n            ");
 				boolean __jte_for_loop_entered_1 = false;
 				for (var link : links) {
 					__jte_for_loop_entered_1 = true;
@@ -41,7 +41,7 @@ public final class JtehomeGenerated {
 				jteOutput.writeContent("\n        </section>\n    </div>\n");
 			}
 		});
-		jteOutput.writeContent("\n\n");
+		jteOutput.writeContent("\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		List<Link> links = (List<Link>)params.get("links");
